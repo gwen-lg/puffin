03237663f0a58571c7dd127ce49e1c5a7d161ac8
Gwen Lg <me@gwenlg.fr>
2022-08-12 18:56:32 +0200

    rename 'puffin server impl' into 'puffin server send'
    this reflect change done when split management

--- puffin_http/src/server.rs
+++ puffin_http/src/server.rs
@@ -250,19 +250,19 @@ impl Server {
                     clients: clients.clone(),
                     num_clients: num_clients_cloned.clone(),
                 };
-                let mut server_impl = PuffinServerImpl {
+                let mut ps_send = PuffinServerSend {
                     clients,
                     num_clients: num_clients_cloned,
                     frame_view: Default::default(),
                 };
 
                 while let Ok(frame) = rx.recv() {
-                    server_impl.frame_view.add_frame(frame.clone());
+                    ps_send.frame_view.add_frame(frame.clone());
                     if let Err(err) = ps_connection.accept_new_clients() {
                         log::warn!("puffin server failure: {}", err);
                     }
 
-                    if let Err(err) = server_impl.send(&frame) {
+                    if let Err(err) = ps_send.send(&frame) {
                         log::warn!("puffin server failure: {}", err);
                     }
                 }
@@ -347,6 +347,7 @@ impl PuffinServerConnection {
                         .context("Couldn't spawn thread")?;
 
                     // Send all scopes when new client connects.
+                    // Bad design is we allready have a client
                     self.clients.write().unwrap().push(Client {
                         client_addr,
                         packet_tx: Some(packet_tx),
@@ -368,13 +369,13 @@ impl PuffinServerConnection {
 }
 
 /// streams to client puffin profiler data.
-struct PuffinServerImpl {
+struct PuffinServerSend {
     clients: Arc<RwLock<Vec<Client>>>,
     num_clients: Arc<AtomicUsize>,
     frame_view: FrameView,
 }
 
-impl PuffinServerImpl {
+impl PuffinServerSend {
     pub fn send(&mut self, frame: &puffin::FrameData) -> anyhow::Result<()> {
         if self.clients.read().unwrap().is_empty() {
             return Ok(());
